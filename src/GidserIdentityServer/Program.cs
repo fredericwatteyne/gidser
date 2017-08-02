2fe631bae4f9fe7d8eb8cec8098dca853e0f17a8
mrericson <mrericson@mrericsons-MacBook-Pro.local>
2017-08-03 01:06:27 +0200

    attempt at fixing build

--- src/GidserIdentityServer/Program.cs
+++ src/GidserIdentityServer/Program.cs
@@ -15,7 +15,6 @@ namespace GidserIdentityServer
 
             var host = new WebHostBuilder()
                 .UseKestrel()
-                .UseUrls("http://localhost:5000")
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
