bd9c236ced29d8668280aca7b4f69a6c91f70b6d
mrericson <mrericson@mrericsons-MacBook-Pro.local>
2017-08-03 00:37:21 +0200

    First commit with inmemory db

---
+++ src/Api/Controllers/RunController.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Api.Controllers
+{
+	[Route("[controller]")]
+    public class RunController : ControllerBase
+    {
+		[HttpGet]
+		public IActionResult Get()
+		{
+            return new JsonResult("Dummy");
+		}
+    }
+}
